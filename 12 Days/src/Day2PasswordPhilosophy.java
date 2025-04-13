f565d74e86d2f6ec37a3ab9eaec71fe9e1233fea
cwcross <cwcross@haverford.edu>
2025-04-12 21:16:45 -0400

    Day 2 Done

--- 12 Days/src/Day2PasswordPhilosophy.java
+++ 12 Days/src/Day2PasswordPhilosophy.java
@@ -5,8 +5,17 @@ import java.io.IOException;
 
 public class Day2PasswordPhilosophy {
     public static void main(String[] args) {
-        String[] input = readFile("Files/Day2.txt");
-        
+        String[] input = readFile("12 Days/Files/Day2.txt");
+        int validPasswords = 0;
+        int newValidPasswords = 0;
+
+        for (String s : input) {
+            if (checkPasswordConditions(findPasswordConditions(s))) validPasswords++;
+            if (checkNewPasswordConditions(findPasswordConditions(s))) newValidPasswords++;
+        }
+
+        System.out.println("Valid passwords: " + validPasswords);
+        System.out.println("New valid passwords: " + newValidPasswords);
     }
     
     public static String[] readFile(String fileName) {
@@ -46,5 +55,26 @@ public class Day2PasswordPhilosophy {
         return password = input.substring(passwordStart);
     }
 
+    public static boolean checkPasswordConditions(ArrayList<String> conditions) {
+        int min = Integer.parseInt(conditions.get(0));
+        int max = Integer.parseInt(conditions.get(1));
+        char character = conditions.get(2).charAt(0);
+        String password = conditions.get(3);
+        int count = 0;
+        for (int i = 0; i < password.length(); i++) {
+            if (password.charAt(i) == character) count++;
+        }
+        return count >= min && count <= max;
+    }
+
+    public static boolean checkNewPasswordConditions(ArrayList<String> conditions) {
+        int pos1 = Integer.parseInt(conditions.get(0));
+        int pos2 = Integer.parseInt(conditions.get(1));
+        char character = conditions.get(2).charAt(0);
+        String password = conditions.get(3);
+
+        if (password.charAt(pos1-1) == character && password.charAt(pos2-1) != character) return true;
+        else return password.charAt(pos1 - 1) != character && password.charAt(pos2 - 1) == character;
+    }
 
 }
