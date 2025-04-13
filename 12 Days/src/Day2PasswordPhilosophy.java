9773cbad159fc77f8ec526b85255226bf45d53c0
cwcross <cwcross@haverford.edu>
2025-04-12 20:56:09 -0400

    Update Day2PasswordPhilosophy.java

--- 12 Days/src/Day2PasswordPhilosophy.java
+++ 12 Days/src/Day2PasswordPhilosophy.java
@@ -1,9 +1,26 @@
+import java.util.ArrayList;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.io.IOException;
+
 public class Day2PasswordPhilosophy {
     public static void main(String[] args) {
+        String[] input = readFile("Files/Day2.txt");
+        
+    }
+    
+    public static String[] readFile(String fileName) {
+        try {
+            return Files.readAllLines(Path.of(fileName)).toArray(new String[0]);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new String[0];
+        }
     }
 
-    public static Object findPasswordCondition(String input) {
-        char character;
+    public static ArrayList<String> findPasswordConditions(String input) {
+        String character;
+        String password;
         String numbers = "";
         for (int i = 0; i < input.length(); i++) {
             if (input.charAt(i) == '-' || Character.isDigit(input.charAt(i))) {
@@ -11,16 +28,23 @@ public class Day2PasswordPhilosophy {
             }
         }
         String[] range = numbers.split("-");
-        int min = Integer.parseInt(range[0]);
-        int max = Integer.parseInt(range[1]);
+        String min = range[0];
+        String max = range[1];
 
         for (int i = 0; true; i++) {
             if (Character.isAlphabetic(input.charAt(i))) {
-                character = input.charAt(i);
+                character = "" + input.charAt(i);
+                password = findPassword(input, i+3);
                 break;
             }
         }
-
-
+        return new ArrayList<String>(java.util.Arrays.asList(min, max, character, password));
     }
+
+    private static String findPassword(String input, int passwordStart) {
+        String password;
+        return password = input.substring(passwordStart);
+    }
+
+
 }
