213b99f5d1f9be99854066434daa5daba4ed27a7
cwcross <cwcross@haverford.edu>
2025-04-21 15:30:26 -0400

    Part 4 Done!

--- 12 Days/src/Day4PassportProcessing.java
+++ 12 Days/src/Day4PassportProcessing.java
@@ -9,6 +9,7 @@ public class Day4PassportProcessing {
         assert passportData != null;
         System.out.println("Total passports: " + passportData.size());
         System.out.println(findCorrectPassports(passportData));
+        System.out.println(findCorrectPassportsPart2(passportData));
     }
 
     public static ArrayList<String[]> readFile(String fileName) {
@@ -74,4 +75,77 @@ public class Day4PassportProcessing {
         }
         return false;
     }
+
+    public static int findCorrectPassportsPart2(ArrayList<String[]> passports) {
+        int correctPassports = 0;
+        for (String[] passport : passports) {
+            if (isValidPassport(passport)) {
+                correctPassports++;
+            }
+        }
+        return correctPassports;
+    }
+
+    private static boolean isValidPassport(String[] passport) {
+        if (passport.length < 7) return false;
+        if (passport.length == 7 && containsCid(passport)) return false;
+
+        for (String field : passport) {
+            String[] keyValue = field.split(":");
+            if (!validateField(keyValue[0], keyValue[1])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+    private static boolean validateField(String key, String value) {
+        try {
+            switch (key) {
+                case "byr":
+                    int byr = Integer.parseInt(value);
+                    return byr >= 1920 && byr <= 2002;
+
+                case "iyr":
+                    int iyr = Integer.parseInt(value);
+                    return iyr >= 2010 && iyr <= 2020;
+
+                case "eyr":
+                    int eyr = Integer.parseInt(value);
+                    return eyr >= 2020 && eyr <= 2030;
+
+                case "hgt":
+                    if (value.endsWith("cm")) {
+                        int cm = Integer.parseInt(value.replace("cm", ""));
+                        return cm >= 150 && cm <= 193;
+                    } else if (value.endsWith("in")) {
+                        int in = Integer.parseInt(value.replace("in", ""));
+                        return in >= 59 && in <= 76;
+                    } else {
+                        return false;
+                    }
+
+                case "hcl":
+                    return value.matches("^#[0-9a-f]{6}$");
+
+                case "ecl":
+                    return value.equals("amb") || value.equals("blu") || value.equals("brn") ||
+                            value.equals("gry") || value.equals("grn") || value.equals("hzl") || value.equals("oth");
+
+                case "pid":
+                    return value.matches("^\\d{9}$");
+
+                case "cid":
+                    return true;
+
+                default:
+                    return false;
+            }
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
 }
